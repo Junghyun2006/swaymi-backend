82aa5452e066a4c15c3921f4297582eaf9df525e
Jung Park <72415771+Junghyun2006@users.noreply.github.com>
2024-03-02 00:43:57 -0500

    implement user registration function

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-require("dotenv").config({ path: ".env.local" });
+// Dependencies
 const express = require("express");
 const { connectDB } = require("./config/mongodb");
 const userRoutes = require("./routes/userRoutes");
+require("dotenv").config({ path: ".env.local" });
 
+// Constants
 const port = parseInt(process.env.PORT, 10) || 3001;
+
+// Connect to Database
 connectDB();
-// const dev = process.env.NODE_ENV !== "production";
+
+// Create Express App
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-
 app.use("/api/users", userRoutes);
 
+// Start the Server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
